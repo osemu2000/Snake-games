1aac8bdb1e8cab4e7ddfba705fc389e68c51c294
osemu2000 <114690388+osemu2000@users.noreply.github.com>
2023-10-05 16:45:50 +0100

    Add files via upload

---
+++ Snakegame.java
@@ -0,0 +1,38 @@
+// import java.util.Random;
+// import java.awt.*;
+// import java.awt.event.*;
+// import javax.swing.*;
+public class Snakegame {
+   public static void main(String args[])
+   {
+      new GameInterface();
+   }
+}
+  
+ 
+//  class GameSettings {
+//     this.add(new GameInterface());
+//     this.Title("SnakeGame");
+//     this.setDefaultCloseOperation(JFrame.Exit_ON_CLOSE);
+//     this.setResizable(false);
+//     this.pack();
+//     this.setVisible(true);
+//     this.setLocationRelativeTo(Null);
+// }
+        // int ScreenX = 600;
+        // int ScreenY = 600;
+        // int GameSpace = 25;
+        // int gameunits = (ScreenX*ScreenY)/GameSpace;
+        // int DELAY = 75;
+
+        // int x[] = new int [gameunits];
+        // int y[] = new int [gameunits];
+
+        // int SnakeBody = 5;
+        // int FoodEaten;
+        // int FoodA;
+        // int FoodB;
+    
+   
+   
+
